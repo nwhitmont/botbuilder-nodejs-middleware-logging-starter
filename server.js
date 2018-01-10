28113bca3f1f8a0b6ec3781b2a9663bc36c3b87e
Nils Whitmont <v-niwh@microsoft.com>
2018-01-10 11:29:27 -0800

    update server

--- server.js
+++ server.js
@@ -2,32 +2,49 @@ const builder = require('botbuilder');
 const restify = require('restify');
 
 const server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log(`${server.name} listening to: ${server.url}`);
-});
-
 const connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-
 server.post('/api/messages', connector.listen());
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log(`${server.name} listening to: ${server.url}`);
+});
 
-const saveAndSendMesage = (session) => {
+const saveAndSendMesage = (session, messageText) => {
     // save conversation-id, message-text, timestamp to db
-    let _timestamp = Date.now();
     let _conversationId = session.conversationId;
-    let _messageTxt = session.message.text;
+    let _timestamp = Date.now(); // use moment.js if you need date formatting
+    // your save to db function goes here:
+    console.log(`[saveMessageAndSend] Saving message with conversationId: ${_conversationId} - messageText: ${messageText} - timestamp: ${_timestamp}`);
+    // saveToDatabase(_conversationId, messageText, _timestamp);
+    // then send the message
+    session.send(messageText);
 };
 
+const saveConversationData = (conversationData) => {
+    console.log(`Got conversationData as: ${conversationData}`); // Debug
+    console.log(`Saving conversationData object...`);
+    // save data
+    console.log(`Done!`);
+}
+
 const bot = new builder.UniversalBot(connector, (session) => {
+    // example: save and send message using custom function 
+    // instead of logging middleware
+    saveAndSendMesage(session, "Save this message and send it...");
+
+    // or call your custom "save conversationData" function before sending message as usual
+    saveConversationData(session.conversationData);
+
+    // then send bot response
     session.send("This is a simple bot.");
 });
 
 // Middleware for logging
 bot.use({
     botbuilder (session, next) {
-        console.log(`[BOTBUIDLER hook] Got session.message.text as: ${session.message.text}`);
+        console.log(`[BOTBUILDER hook] Got session.message.text as: ${session.message.text}`);
         next();
       },
     receive: function (event, next) {
